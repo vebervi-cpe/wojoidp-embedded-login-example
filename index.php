8449eda153f911487584f558d79179a206aaf235
vebervi-cpe <61085569+vebervi-cpe@users.noreply.github.com>
2022-04-28 11:09:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -239,7 +239,7 @@
 				}
 				    
 				xhrLogin.send("username=" + encodeURIComponent(email) + "&password=" + encodeURIComponent(password) + 
-					      "&startURL=" + encodeURIComponent("<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>") +
+					      "&startURL=" + encodeURIComponent("/services/oauth2/authorize?response_type=token&client_id=3MVG9SOw8KERNN09PRbxcQXSU8sP0XUQwqFhCEPXfu.jDd507D8ntaakmX7J.WDIZZ4F2_UV6aEdbjcz0qiib&redirect_uri=https%3A%2F%2Fwojoidp.herokuapp.com%2F_callback.php&state=https%3A%2F%2Fwojoidp.herokuapp.com%2F") +
 					      "&mode=" + encodeURIComponent("<?php echo getenv('SALESFORCE_MODE');?>") +
 					      "&maskRedirects=" + encodeURIComponent("<?php echo getenv('SALESFORCE_MASK_REDIRECTS');?>"))				    
 			    }, 3500);
